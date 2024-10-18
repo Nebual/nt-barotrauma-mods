da39e1486e05007ccb62a3890ed95b92dd190303
Nebual <ben1120@gmail.com>
2024-10-17 18:52:58 -0700

    RadioBroadcast: don't let non-characters broadcast

--- RadioBroadcast/Lua/Autorun/init.lua
+++ RadioBroadcast/Lua/Autorun/init.lua
@@ -2,6 +2,13 @@ print("Initializing Radio Broadcast")
 
 local BROADCAST_CHANNEL = 9
 
+local function isOwnerCharacter(itemComponent)
+	return itemComponent.Item
+		and itemComponent.Item.ParentInventory
+		and itemComponent.Item.ParentInventory.Owner
+		and LuaUserData.IsTargetType(itemComponent.Item.ParentInventory.Owner, "Barotrauma.Character")
+end
+
 Hook.Patch('RadioBroadcastOverride', 'Barotrauma.Items.Components.WifiComponent', 'CanReceive', function (instance, ptable)
 	local sender = ptable['sender']
 	local recipient = instance
@@ -11,7 +18,11 @@ Hook.Patch('RadioBroadcastOverride', 'Barotrauma.Items.Components.WifiComponent'
 		sender = instance
 		recipient = ptable['sender']
 	end
-	if (sender.Channel == BROADCAST_CHANNEL and recipient.Channel <= 9) or (recipient.Channel == BROADCAST_CHANNEL and sender.Channel <= 9) then
+	if  (
+			(sender.Channel == BROADCAST_CHANNEL and recipient.Channel <= 9)
+			or (recipient.Channel == BROADCAST_CHANNEL and sender.Channel <= 9)
+		) and isOwnerCharacter(sender)
+	then
 		-- allow broadcast channel to hear/speak to channels 0-9. If a player wants to be secret they can go >= 10
 		ptable.PreventExecution = true
 		return true
